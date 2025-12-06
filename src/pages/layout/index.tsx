c64e56bc54d7ca95b0abb8f727d90ff45f9361e4
hzx <746206915@qq.com>
2025-12-06 15:55:14 +0800

    菜单使用本地数据

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -4,11 +4,10 @@ import type { FC } from 'react';
 import './index.less';
 
 import { Drawer, Layout, theme as antTheme } from 'antd';
-import { Suspense, useCallback, useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useLocation } from 'react-router';
 
-import { getMenuList } from '@/api/layout.api';
 import { setUserItem } from '@/stores/user.store';
 import { getFirstPathCode } from '@/utils/getFirstPathCode';
 import { getGlobalState } from '@/utils/getGloabal';
@@ -16,6 +15,166 @@ import { getGlobalState } from '@/utils/getGloabal';
 import HeaderComponent from './header';
 import MenuComponent from './menu';
 import TagsView from './tagView';
+
+const sssssMenuList: MenuList = [
+  {
+    code: 'dashboard',
+    label: {
+      zh_CN: '首页',
+      en_US: 'Dashboard',
+    },
+    icon: 'dashboard',
+    path: '/dashboard',
+  },
+  {
+    code: 'account',
+    label: {
+      zh_CN: '个人中心',
+      en_US: 'Personal Center'
+    },
+    icon: 'account', // 确保CustomIcon中有此图标
+    path: '/account'
+  },
+  {
+    code: 'permission',
+    label: {
+      zh_CN: '权限',
+      en_US: 'Permission',
+    },
+    icon: 'permission',
+    path: '/permission',
+    children: [
+      {
+        code: 'routePermission',
+        label: {
+          zh_CN: '路由权限',
+          en_US: 'Route Permission',
+        },
+        path: '/permission/route',
+      },
+      {
+        code: 'notFound',
+        label: {
+          zh_CN: '404',
+          en_US: '404',
+        },
+        path: '/permission/404',
+      },
+    ],
+  },
+  {
+    code: 'component',
+    label: {
+      zh_CN: '组件',
+      en_US: 'Component',
+    },
+    icon: 'permission',
+    path: '/component',
+    children: [
+      {
+        code: 'componentForm',
+        label: {
+          zh_CN: '表单',
+          en_US: 'Form',
+        },
+        path: '/component/form',
+      },
+      {
+        code: 'componentTable',
+        label: {
+          zh_CN: '表格',
+          en_US: 'Table',
+        },
+        path: '/component/table',
+      },
+      {
+        code: 'componentSearch',
+        label: {
+          zh_CN: '查询',
+          en_US: 'Search',
+        },
+        path: '/component/search',
+      },
+      {
+        code: 'componentAside',
+        label: {
+          zh_CN: '侧边栏',
+          en_US: 'Aside',
+        },
+        path: '/component/aside',
+      },
+      {
+        code: 'componentTabs',
+        label: {
+          zh_CN: '选项卡',
+          en_US: 'Tabs',
+        },
+        path: '/component/tabs',
+      },
+      {
+        code: 'componentRadioCards',
+        label: {
+          zh_CN: '单选卡片',
+          en_US: 'Radio Cards',
+        },
+        path: '/component/radio-cards',
+      },
+    ],
+  },
+
+  {
+    code: 'business',
+    label: {
+      zh_CN: '业务',
+      en_US: 'Business',
+    },
+    icon: 'permission',
+    path: '/business',
+    children: [
+      {
+        code: 'basic',
+        label: {
+          zh_CN: '基本',
+          en_US: 'Basic',
+        },
+        path: '/business/basic',
+      },
+      {
+        code: 'withSearch',
+        label: {
+          zh_CN: '带查询',
+          en_US: 'WithSearch',
+        },
+        path: '/business/with-search',
+      },
+      {
+        code: 'withAside',
+        label: {
+          zh_CN: '带侧边栏',
+          en_US: 'WithAside',
+        },
+        path: '/business/with-aside',
+      },
+      {
+        code: 'withRadioCard',
+        label: {
+          zh_CN: '带单选卡片',
+          en_US: 'With Nav Tabs',
+        },
+        path: '/business/with-radio-cards',
+      },
+      {
+        code: 'withTabs',
+        label: {
+          zh_CN: '带选项卡',
+          en_US: 'With Tabs',
+        },
+        path: '/business/with-tabs',
+      },
+    ],
+  },
+  // 可根据需要添加其他菜单项
+];
 
 const { Sider, Content } = Layout;
 const WIDTH = 992;
@@ -62,22 +221,14 @@ const LayoutPage: FC = () => {
     return MenuListAll;
   };
 
-  const fetchMenuList = useCallback(async () => {
-    const { status, result } = await getMenuList();
-
-    if (status) {
-      setMenuList(result);
-      dispatch(
-        setUserItem({
-          menuList: initMenuListAll(result),
-        }),
-      );
-    }
-  }, [dispatch]);
-
   useEffect(() => {
-    fetchMenuList();
-  }, [fetchMenuList]);
+    setMenuList(sssssMenuList);
+    dispatch(
+      setUserItem({
+        menuList: initMenuListAll(sssssMenuList),
+      }),
+    );
+  }, [dispatch]);
 
   useEffect(() => {
     window.onresize = () => {
